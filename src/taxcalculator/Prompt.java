0d76f51fa823cb6e793f4d8ee9061ce7a0f9df2b
Sander2022164 <2022164@student.cct.ie>
2023-11-26 09:23:31 +0000

    Methods being created
    Thoughts about the methods and how to implement them

--- src/taxcalculator/Prompt.java
+++ src/taxcalculator/Prompt.java
@@ -13,17 +13,52 @@ import java.util.Scanner;
 //Creating prompt which will ask for User's input
 public class Prompt {
     
+    public String userName;
+    public String password;
+    
     //Adding scanner to get users input
     Scanner mykb = new Scanner(System.in);
-
-    //Method will get user input and validate
+    //Method that will display the welcome message 
+    
+    public void Welcome(){
+        //Displaying welcome message
+        System.out.println("Hi!! Welcome to the TAX calculator!\n"
+                + "Please, provide your credentials!\n"
+                + "Are you an Administrator or a User?\n"
+                + "1.Administrator\n"
+                + "2.User");
+        
+    }
+    
+    public void TypeOfUser(){
+            int typeOfUser = mykb.nextInt();
+            
+            if(typeOfUser==1){
+                System.out.println("Please enter your credentials.");
+            }else if(typeOfUser==2){
+                System.out.println("Welcome User. Please sign up:");
+            }else{
+                System.out.println("Please enter the numbers 1 or 2.");
+            }
+            
+    }
+    
+    //Method will get user surname
     public void UserNameImput(){
-         String userName = mykb.nextLine();
-         String password = mykb.nextLine();
-        if (userName =="CCT"){
-            System.out.println("Please enter your password CCT Administrator");
+        //Ask user for username
+        System.out.println("UserName: ");
+        //Collecting user's input
+        String userName = mykb.nextLine();
+        //IF user input is CCT, asks for password
+        System.out.println("Password: ");
+        if (userName=="CCT"){
+            System.out.println("Password: ");
+            String password = mykb.nextLine();
+            if (password=="Dublin"){
+                System.out.println("Welcome CCT Administrator");
+            }
         }else{
-            System.out.println("Welcome student +");
+            System.out.println("Welcome Student "+ userName);
         }
     }
     
--- src/taxcalculator/TaxCalculator.java
+++ src/taxcalculator/TaxCalculator.java
@@ -16,18 +16,18 @@ public class TaxCalculator {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //Displaying welcome message
-        System.out.println("Hi!! Welcome to the TAX calculator!/n"
-                + "Please, provide your credentials!/n"
-                + "Username:");
+        Prompt one = new Prompt();
+        one.Welcome();;
+        one.TypeOfUser();
         
-        Employee junior = new Employee ("junior", "rodrigues",45000, 2000);
         
-        System.out.println(junior.getOver());
-        System.out.println(junior.getPaye());
-        System.out.println(junior.getPrsi());
-        System.out.println(junior.getOwn());
-        System.out.println(junior.showCalculation());
+//        Employee junior = new Employee ("junior", "rodrigues",45000, 2000);
+//        
+//        System.out.println(junior.getOver());
+//        System.out.println(junior.getPaye());
+//        System.out.println(junior.getPrsi());
+//        System.out.println(junior.getOwn());
+//        System.out.println(junior.showCalculation());
     }
     
 }
