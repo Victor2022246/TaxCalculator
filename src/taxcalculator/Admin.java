fda740238134026d57333b611cce1e0794282d1c
Sander2022164 <2022164@student.cct.ie>
2023-11-26 11:05:33 +0000

    Class Admin created
    Class Admin created, in order to add what the admin can do

---
+++ src/taxcalculator/Admin.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taxcalculator;
+
+/**
+ *
+ * @author sande
+ */
+public class Admin {
+    
+    private String firstName;
+    private String surName;
+    private String age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+    
+    
+}
