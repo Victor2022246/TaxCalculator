2c0382906c4882385b2a6f30f7cd6e217b44ba13
Sander2022164 <2022164@student.cct.ie>
2023-11-27 19:12:07 +0000

    Employee Class implemented

--- src/taxcalculator/Employee.java
+++ src/taxcalculator/Employee.java
@@ -8,122 +8,31 @@ package taxcalculator;
  *
  * @author victor pc
  */
-public class Employee {
+public class Employee extends TaxFees{
+    
     private String name;
     private String surname;
     private int employeeID;
-    private int salary;
-    private int taxCredits;
-    private double paye;
-    private double ucs;
-    private double prsi;
-    private double own;
+    private double grossSalary;
+    private double netSalary;
     private static int currentID=1;
-      int over = salary - 40000;
-    
-     
-     public Employee(String name, String surname,  int salary, int taxCredits) {
-        this.over =salary - 40000;
-          if(over < 0){ this.over = 0 ;} // if the overtime is no positive so its set for been 0 
-         this.name = name;
-        this.surname = surname;
-        this.employeeID = currentID;
-        this.salary = salary;
-        this.taxCredits = taxCredits;
-        this.paye = ((salary - over) * 0.20 ) + (over * 0.40)  ; // calculate the paye basede on 20% fot the amount under 40000 + 40% of the amount over 40000
-        this.ucs = salary * 0.05;
-        this.prsi = salary * 0.03;
-        this.own= ( paye + ucs + prsi - taxCredits);
-       
-        
-        currentID++;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public int getEmployeeID() {
-        return employeeID;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public int getTaxCredits() {
-        return taxCredits;
-    }
-
-    public double getPaye() {
-        return paye;
-    }
-
-    public double getUcs() {
-        return ucs;
-    }
-
-    public double getPrsi() {
-        return prsi;
-    }
-
-    public double getOwn() {
-        return own;
-    }
-
-    public int getOver() {
-        return over;
-    }
-    
-
-    public static int getCurrentID() {
-        return currentID;
-    }
-
-    public void setName(String name) {
+    public Employee(String name, String surname, double grossSalary) {
         this.name = name;
-    }
-
-    public void setSurname(String surname) {
         this.surname = surname;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public void setTaxCredits(int taxCredits) {
-        this.taxCredits = taxCredits;
+        this.grossSalary = grossSalary;
+        this.netSalary = (grossSalary-(grossSalary*PAYE*USC*PRSI));//The net salary will be after all deduction
+        this.employeeID = currentID;//EmployeeId will start as current ID(value 1)
+        currentID++;//Increasing one everytime we create an employee
     }
     
-    
-    public double showCalculation(){
-        System.out.println("hello mr(s) " + name );
-        System.out.println(" this are youy tax calculation");
-        System.out.println("Your gross salary is " + salary + "and yor tax credits are " + taxCredits);
-        System.out.println("The paye tax based on a 20% rate over 40 thousands + 40% over the overtime is " + paye);
-        System.out.println("the UCS base on a rate of 2% is " + ucs + " and the PRSI based on 3% is " + prsi);
-        System.out.println(" Your own for the government");
-    return own;
+    //Method to show the Taxes' deductions
+    public String Salary(){
+        System.out.println("Hello " + name );
+        System.out.println("Gross Salary " + grossSalary);
+        System.out.println("TAX PAYE(20%) "+ grossSalary*PAYE);
+        System.out.println("TAX USC(5%) " + grossSalary*USC);
+        System.out.println("TAX PRSI(3%) " + grossSalary*PRSI);
+    return "NetSalary = " + netSalary;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
