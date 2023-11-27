4e6c0be866dafb599b92f346d5d57e26d6965d97
Sander2022164 <2022164@student.cct.ie>
2023-11-27 18:33:36 +0000

    Commenting our Database Class
    Commenting database abstract class, adding protected to the variables

--- src/taxcalculator/Database.java
+++ src/taxcalculator/Database.java
@@ -9,15 +9,21 @@ package taxcalculator;
  * @author victor pc
  */
 public abstract class Database {
-    
-    final static String DB_BASE_URL= "jdbc:mysql://localhost";
-    final static String USER = "ooc2023";
-    final static  String PASSWORD= "ooc2023";
+     //java database connector:mysql (specifies the protocol and the host)
+    //The base URL For connecting to the MySQL Database
+    protected final static String DB_BASE_URL= "jdbc:mysql://localhost";
+    //Username to be used for connecting to the MySQL database
+    protected final static String USER = "ooc2023";
+    //The password to be used for connecting to the MySQL DB
+    protected final static  String PASSWORD= "ooc2023";
+    //Declaring the database's name
     protected final static String DB_NAME = "taxation";
+    //A table name that will be use to store employee Data
     protected final static String TABLE_NAME= "employeeData";
    
-    //jdbc:mysql://localhost/hospital
+    //jdbc:mysql://localhost/taxation
     protected final static String DB_URL = DB_BASE_URL + "/" + DB_NAME;
+   
     
 }
 
