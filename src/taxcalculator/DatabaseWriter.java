784199e192dc19d6a8dd1581512dba52543cf4c1
Sander2022164 <2022164@student.cct.ie>
2023-11-28 12:17:43 +0000

    DatabaseWriter class created
    DatabaseWriter has been created, adding our the steps in order to add a employee into a table

---
+++ src/taxcalculator/DatabaseWriter.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taxcalculator;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+/**
+ *
+ * @author sande
+ */
+public class DatabaseWriter extends Database{
+    
+    public boolean addEmployee(Employee employee) throws SQLException{
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);  
+                Statement stmt= conn.createStatement();
+                ){
+                    String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
+                       + "%d,'%s','%s', %d, %d);",
+                          employee.getEmployeeID(), employee.getName(), employee.getSurname(), employee.getGrossSalary(), employee.getNetSalary());
+                  stmt.execute(sql);
+                  return true;
+    }catch(Exception e){
+        e.printStackTrace();
+                  return false;
+    }
+    }
+    public boolean addAllEmployee(List<Employee> employeeList){
+        return true;
+}
+}
