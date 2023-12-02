6cc7ba6d30926d0d42db2835878271726f5c4122
Sander2022164 <2022164@student.cct.ie>
2023-12-02 18:20:53 +0000

    Creating again database with new information

--- src/taxcalculator/TaxCalculator.java
+++ src/taxcalculator/TaxCalculator.java
@@ -19,29 +19,29 @@ public class TaxCalculator {
         // TODO code application logic here
   
         
-        //DATA BASE CONNECTION WAS SUCCESFULL
-         if(DatabaseSetup.setupDB()){
-            System.out.println("Database and Table created");
-        }else{
-            System.out.println("Oh no! There was a database creation problem...");
-        }
-         
-        Employee emp1 = new Employee("Sander", "Soares", 10000, 12000, "Sansan", "9876");
-        Employee emp2 = new Employee("Victor", "Oliveira", 50000, 12000, "vic" , "vic123");
-        Employee emp3 = new Employee("Kevin", "Santos", 27000, 12000, "kevins", "keke123");
-        Employee emp4 = new Employee("Renato", "Ramos", 42000, 12000, "Rere", "Re123");
-        Employee emp5 = new Employee("Felipe", "Paiva", 39000, 12000, "Rere", "re123");
-        
-        DatabaseWriter dbw = new DatabaseWriter();
-        dbw.addEmployee(emp1);
-        dbw.addEmployee(emp2);
-        dbw.addEmployee(emp3);
-        dbw.addEmployee(emp4);
-        dbw.addEmployee(emp5);
+//        //DATA BASE CONNECTION WAS SUCCESFULL
+//         if(DatabaseSetup.setupDB()){
+//            System.out.println("Database and Table created");
+//        }else{
+//            System.out.println("Oh no! There was a database creation problem...");
+//        }
+//         
+//        Employee emp1 = new Employee("Sander", "Soares", 10000, 12000, "Sansan", "9876");
+//        Employee emp2 = new Employee("Victor", "Oliveira", 50000, 12000, "vic" , "vic123");
+//        Employee emp3 = new Employee("Kevin", "Santos", 27000, 12000, "kevins", "keke123");
+//        Employee emp4 = new Employee("Renato", "Ramos", 42000, 12000, "Rere", "Re123");
+//        Employee emp5 = new Employee("Felipe", "Paiva", 39000, 12000, "Rere", "re123");
+//        
+//        DatabaseWriter dbw = new DatabaseWriter();
+//        dbw.addEmployee(emp1);
+//        dbw.addEmployee(emp2);
+//        dbw.addEmployee(emp3);
+//        dbw.addEmployee(emp4);
+//        dbw.addEmployee(emp5);
 
-//        Prompt p1 = new Prompt();
-//        p1.Welcome();
-//        p1.UserValidation();
+        Prompt p1 = new Prompt();
+        p1.Welcome();
+        p1.UserValidation();
         
 
 //        
