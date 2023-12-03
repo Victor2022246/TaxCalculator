8470185a7459b0e87255ca6358fb881426486034
Sander2022164 <2022164@student.cct.ie>
2023-12-03 11:54:14 +0000

    Commenting writer
    Commenting the writer methods

--- src/taxcalculator/DatabaseWriter.java
+++ src/taxcalculator/DatabaseWriter.java
@@ -13,18 +13,28 @@ import java.util.List;
 /**
  *
  * @author sande
+ * The DatabaseWriter class is responsible for writing data to the database
+ * extending the common database-related information from Database abstract class
  */
 public class DatabaseWriter extends Database{
-    
+    /**
+     * Adds a new employee to our DB
+     * @param employee The Employee object to be added to the DB
+     * @return true if employee was successfully added, false otherwise
+     * @throws SQLException if there is any error
+     */
     public boolean addEmployee(Employee employee) throws SQLException{
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);  
                 Statement stmt= conn.createStatement();
                 ){
+                    //Constructing the query to be inserted
+                    //%d for integers, '%s' for strings and %f for double/float
+                    //using get methods to get all info from employee in Employee's class
                     String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
                        + "%d,'%s','%s', %f, %f, %f, %f, %f, %f, %f, '%s', '%s');",
                           employee.getEmployeeID(), employee.getName(), employee.getSurname(), employee.getGrossSalary(),employee.getTaxCredit(), employee.getPAYE(),
                           employee.getUSC(), employee.getPRSI(),employee.getTotalOwe(), employee.getNetSalary(), employee.getUsername(), employee.getPassword());
-                  stmt.execute(sql);
+                  stmt.execute(sql);//Execute the query above
                   return true;
     }catch(Exception e){
         e.printStackTrace();
@@ -34,10 +44,19 @@ public class DatabaseWriter extends Database{
     public boolean addAllEmployee(List<Employee> employeeList){
         return true;
 }
+    /**
+     * Update a specif field in our DB's table
+     * @param userName The username of the employee requesting the update
+     * @param field the column that will be updated
+     * @param newValue the new value entered by the user
+     * @return true if the update was successful, false otherwise
+     * @throws Exception If occurs any errors
+     */
     public boolean updateEmploye (String userName,String field,Object newValue){
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);  
                 Statement stmt= conn.createStatement();
                 ){
+            //Declaring the variable outisde so can be used later on
             String sql;
                     if (newValue instanceof String){
                         sql = String.format("UPDATE %s SET %s='%s' WHERE username='%s';", TABLE_NAME, field, newValue, userName);
