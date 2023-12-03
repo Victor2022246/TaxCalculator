2ccff89d4f282bfa44aa0946051404520f64f73b
Sander2022164 <2022164@student.cct.ie>
2023-12-03 11:14:07 +0000

    Creating new Writer method
    Method will be responsible for editing the user's option

--- src/taxcalculator/DatabaseWriter.java
+++ src/taxcalculator/DatabaseWriter.java
@@ -34,4 +34,19 @@ public class DatabaseWriter extends Database{
     public boolean addAllEmployee(List<Employee> employeeList){
         return true;
 }
+    public void updateEmploye (){
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);  
+                Statement stmt= conn.createStatement();
+                ){
+                    String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
+                       + "%d,'%s','%s', %f, %f, %f, %f, %f, %f, %f, '%s', '%s');",
+                          employee.getEmployeeID(), employee.getName(), employee.getSurname(), employee.getGrossSalary(),employee.getTaxCredit(), employee.getPAYE(),
+                          employee.getUSC(), employee.getPRSI(),employee.getTotalOwe(), employee.getNetSalary(), employee.getUsername(), employee.getPassword());
+                  stmt.execute(sql);
+                  return true;
+    }catch(Exception e){
+        e.printStackTrace();
+                  return false;
+    }
+    }
 }
