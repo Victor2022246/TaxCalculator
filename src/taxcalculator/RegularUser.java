7d715c4e5da00f809f0e338ae3ddb3e180903497
Sander2022164 <2022164@student.cct.ie>
2023-12-03 10:28:45 +0000

    User's options
    Regular User's options will be displayed, asking for what info wants to change and then display that the change has been successfully

--- src/taxcalculator/RegularUser.java
+++ src/taxcalculator/RegularUser.java
@@ -4,6 +4,8 @@
  */
 package taxcalculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sande
@@ -43,6 +45,7 @@ public class RegularUser {
     //Regular user data is outputed
     
     public void regularUserInfo(){
+        
         System.out.println("Would you like to change any information:\n"
                 + "1.Name\n"
                 + "2.Surname\n"
@@ -50,6 +53,54 @@ public class RegularUser {
                 + "4.TaxCredit\n"
                 + "5.Username\n"
                 + "6.Password");
+        
+        Scanner sc = new Scanner(System.in);
+        DatabaseWriter dbWriter = new DatabaseWriter();
+        
+        int choice = sc.nextInt();
+        sc.nextLine();//Cleaning input request
+        
+        switch(choice){
+            case 1:
+                System.out.println("Enter new name: ");
+                String newName = sc.nextLine().trim();
+                System.out.println("Name has been updated!");
+                break;
+                
+            case 2:
+                System.out.println("Enter new Surname: ");
+                String newSurname = sc.nextLine().trim();
+                System.out.println("Surname has been updated!");
+                break;
+                
+            case 3:
+                System.out.println("Enter new Gross Salary:");
+                double newGrossSalary = sc.nextDouble();
+                System.out.println("Gross Salary has been updated!");
+                break;
+                
+            case 4:
+                System.out.println("Enter new Tax Credit:");
+                double newTaxCredit = sc.nextDouble();
+                System.out.println("Tax Credit has been updated!");
+                break;
+                
+            case 5:
+                System.out.println("Enter new UserName:");
+                String newUsername = sc.nextLine().trim().toUpperCase();
+                System.out.println("Username has been updated!");
+                break;
+                
+            case 6:
+                System.out.println("Enter new password:");
+                String newPassword = sc.nextLine();
+                System.out.println("Password has been updated!");
+                break;
+                
+            default:
+                System.out.println("Invalid choice, please pick an option between 1 and 6.");
+                break;
+        }
     }
     
 }
