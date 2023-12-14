78d898dc96d9a8163729bd44383063a4322d6049
Victor2022246 <146198699+SanderSSoares@users.noreply.github.com>
2023-12-14 17:37:25 +0000

    Commenting taxFess
    Added a few comments on TaxFees

--- src/taxcalculator/TaxFees.java
+++ src/taxcalculator/TaxFees.java
@@ -11,7 +11,7 @@ package taxcalculator;
 public abstract class TaxFees {
     //Declaring in this abstract class make it easier in case we want to change it in the future.
     //Tax Credits variables
-    public double PAYE;
-    public double USC =  0.05;
-    public double PRSI =  0.03; 
+    public double PAYE;//Paye will depend on how much the Gross salary is. The whole math is presented in Employee's class
+    public double USC =  0.05;//USC will be set as a fixed 5%
+    public double PRSI =  0.03; //PRSI will be set as a fixed 3%
 }
