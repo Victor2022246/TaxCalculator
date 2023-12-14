ad202aa2c4d88f31177487425cbb98ac6a53e25e
Victor2022246 <146198699+SanderSSoares@users.noreply.github.com>
2023-12-14 17:52:01 +0000

    Commenting Admin class
    Adding comments do admin class

--- src/taxcalculator/Admin.java
+++ src/taxcalculator/Admin.java
@@ -9,28 +9,39 @@ import java.util.Scanner;
 /**
  *
  * @author sande
+ * Class that will contain everything Administrator can do
  */
 public class Admin {
+    //Declaring admin's credentials
     private String userName = "CCT";
     private String password = "Dublin";
+    //Storing choice input by ADmin
     private int choice;
+    //Instantiating the scanner
     Scanner sc = new Scanner(System.in);
+    //Instantiating the Reader and Writer
     DatabaseReader dbr = new DatabaseReader();
     DatabaseWriter dbw = new DatabaseWriter();
    
+    /**
+     * Method that will have the administrator's menu
+     * @throws Exception 
+     */
     public void adminMenu() throws Exception{
-        //Looping the process until Amdin hits option 5
+        //Looping the process until Amdin hits option 4
+        //Initialiating do-while loop
        do{ 
         System.out.println("What would you like to do? \n"
                 + "1.Change youw own information\n"
                 + "2. Access list of users\n"
                 + "3. Remove users\n"
                 + "4. Finish");
-        choice = sc.nextInt();
-        sc.nextLine();
-        
+        choice = sc.nextInt();//Storing admin's choice after request
+        sc.nextLine();//cleaning input buffer
+        //Starting switch statement
          switch(choice){
-            case 1: 
+            case 1:
+            //Case 1, admin wants to change its own information
             //Calling the method that was alredy created in Regular user that will display
             //All info based in login and password provided
                 RegularUser ru = new RegularUser("CCT", "Dublin");//Giving the parameters, once Admin username and password are just one
@@ -39,35 +50,37 @@ public class Admin {
                 break;
                 
             case 2:
+                //Case 2: Admin wants to access list of users and all details
                 System.out.println("Follow List of User");
                 System.out.println("----------------------------------------------------");
-                dbr.displayAllData();
+                dbr.displayAllData();//Calling method that will display list of users
                 break;
                 
             case 3:
+                //Case3: admin wants to remove an user
+                //Prmpt that will ask for the employeeID of the staff he wants to remove
                 System.out.println("Which user would you like to delete?\n"
                         + "Please enter his/her EmployeeID and Press Enter");
-                
+                //Storing the input
                 int deleteEmployee = sc.nextInt();
-                sc.nextLine();
-                
-                boolean delete = dbw.deleteEmployee(deleteEmployee);
-                
+                sc.nextLine();//cleaning input buffers
+                boolean delete = dbw.deleteEmployee(deleteEmployee);//Calling deleteEmployee method               
                 if (delete){
-                    System.out.println("Employee has been deleted!");
+                    System.out.println("Employee has been deleted!");//If everything was fine, display user was deleted
                 }else{
-                    System.out.println("Error Deleting employee. Please try again.");
+                    System.out.println("Error Deleting employee. Please try again.");//Throw error message otherwise
                 }
                 break;
                 
             case 4:
-                System.out.println("Program Closed!!!");
+                //Case 4: admin wants to finish the program
+                System.out.println("Program Closed!!!");//Program finishes after displaying this.
                 break;
 
             default:
-                System.out.println("Invalid choice, please pick an option between 1 and 4.");
+                System.out.println("Invalid choice, please pick an option between 1 and 4.");//In case user does not input valid option
         
     }
-       }while(choice!=4);//Program will loop until choices are different than 5
+       }while(choice!=4);//Program will loop until choices are different than 4
     }
 }
